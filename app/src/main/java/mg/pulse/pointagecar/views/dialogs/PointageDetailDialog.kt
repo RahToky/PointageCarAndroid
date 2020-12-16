1476cf8562f8de6cad959f2974cb60c9346f9000
Mahatoky_PULSE <mahatoky.rasolonirina@pulse.mg>
2020-12-16 14:33:38 +0300

    ajout possibilité d' appel/sms collaborateur

--- app/src/main/java/mg/pulse/pointagecar/views/dialogs/PointageDetailDialog.kt
+++ app/src/main/java/mg/pulse/pointagecar/views/dialogs/PointageDetailDialog.kt
@@ -1,10 +1,13 @@
 package mg.pulse.pointagecar.views.dialogs
 
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import mg.pulse.pointagecar.R
@@ -17,6 +20,8 @@ class PointageDetailDialog : DialogFragment() {
     private var nomTv: TextView? = null
     private var prenomTv: TextView? = null
     private var telephoneTv: TextView? = null
+    private var smsBtn:ImageButton? = null
+    private var callBtn:ImageButton? = null
 
     private var collaborateur: Collaborateur? = null
 
@@ -27,6 +32,7 @@ class PointageDetailDialog : DialogFragment() {
     ): View? {
         val rootView: View = inflater.inflate(R.layout.dialog_pointage_detail, container, false);
         initViews(rootView)
+        initListeners(rootView)
         if(collaborateur != null){
             displayCollaborateur()
         }
@@ -51,11 +57,28 @@ class PointageDetailDialog : DialogFragment() {
         telephoneTv?.text = formatPhoneNumberMG(collaborateur?.telephone)
     }
 
+    private fun initListeners(view: View){
+        callBtn?.setOnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:${collaborateur?.telephone}"))
+            startActivity(intent);
+        }
+
+        smsBtn?.setOnClickListener {
+            val uri = Uri.parse("smsto:${collaborateur?.telephone}")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            intent.putExtra("sms_body", "Bonjour ${collaborateur?.nom},\n")
+            startActivity(intent)
+        }
+    }
+
     private fun initViews(view: View) {
         matriculeTv = view.findViewById(R.id.matriculeTv)
         nomTv = view.findViewById(R.id.nomTv)
         prenomTv = view.findViewById(R.id.prenomTv)
         telephoneTv = view.findViewById(R.id.telephoneTv)
+        smsBtn = view.findViewById(R.id.smsBtn)
+        callBtn = view.findViewById(R.id.callBtn)
     }
 
     fun setCollaborateur(collaborateur: Collaborateur){
